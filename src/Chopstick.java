b57536bc1045c9e55cb56d63d013dfd3ffa38fdf
nwraming1 <nwraming1@catamount.wcu.edu>
2017-04-28 17:13:52 -0400

    Finished up the chopstick class

--- src/Chopstick.java
+++ src/Chopstick.java
@@ -4,10 +4,11 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Nathan W. Raming
- * CS ___ : _________
- * Project _ : _______
- * Class Name: ________
- * Description:
+ * CS 370 : Operating Systems
+ * Project 4 : Dining Philosophers
+ * Class Name: Chopstick.java
+ * Description: This is an Class that represents the Chopstick object. The
+ * philosophers must used this "shared memory" to eat at correct times.
  *
  * @version 1.0
  * @date 28.Apr.2017
@@ -20,11 +21,43 @@ public class Chopstick
 
     protected static final Condition INUSE = lock.newCondition();
 
-    protected AtomicBoolean used;
+    protected AtomicBoolean inUse;
 
     public Chopstick()
     {
-        used = new AtomicBoolean();
-        used.getAndSet(false);
+        inUse = new AtomicBoolean();
+        inUse.getAndSet(false);
+    }
+
+    protected void getChopstick()throws InterruptedException
+    {
+        lock.lock();
+        if(!inUse.get())
+        {
+            inUse.getAndSet(true);
+            AVALIABLE.signalAll();
+        }
+        else
+        {
+            try {
+                INUSE.await();
+                inUse.getAndSet(true);
+            }
+            catch(InterruptedException ie)
+            {
+                throw new InterruptedException(ie.getMessage());
+            }
+        }
+        lock.unlock();
+    }
+
+    protected void replaceChopstick()
+    {
+        lock.lock();
+
+        inUse.getAndSet(false);
+        INUSE.signalAll();
+
+        lock.unlock();
     }
 }
